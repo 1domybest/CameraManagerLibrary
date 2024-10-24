93b004c91dd2441c940672272d433c14ece95b11
OTIS <72382663+1domybest@users.noreply.github.com>
2024-10-24 18:01:11 +0900

    add new Project
    add new Project

---
+++ Frameworks/CameraManagerFrameWork.xcframework/ios-arm64/CameraManagerFrameWork.framework/Headers/CameraManagerFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  CameraManagerFrameWork.h
+//  CameraManagerFrameWork
+//
+//  Created by 온석태 on 10/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraManagerFrameWork.
+FOUNDATION_EXPORT double CameraManagerFrameWorkVersionNumber;
+
+//! Project version string for CameraManagerFrameWork.
+FOUNDATION_EXPORT const unsigned char CameraManagerFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraManagerFrameWork/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "CameraManagerLibrary",
+    platforms: [
+        .iOS(.v14) // 지원하는 최소 플랫폼 버전 설정
+    ],
+    products: [
+        .library(
+            name: "CameraManagerLibrary",
+            targets: ["CameraManagerLibrary"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "CameraManagerFrameWork", // .xcframework 타겟 참조
+            path: "Frameworks/CameraManagerFrameWork.xcframework" // .xcframework 경로
+        ),
+        .target(
+            name: "CameraManagerLibrary",
+            dependencies: [
+                .target(name: "CameraManagerFrameWork") // .xcframework 링크
+            ],
+            path: "Sources/CameraManagerLibrary",
+            resources: [
+                .process("CameraMetalShader.metallib") // .metallib 파일 추가
+            ]
+        ),
+    ]
+)
